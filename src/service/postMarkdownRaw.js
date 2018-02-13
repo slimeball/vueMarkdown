a5d5a4697b0413c1cae8d2aa99ad8e03ec4cd3d0
slimeball <slimeball@foxmail.com>
2018-02-14 02:37:13 +0800

    解决跨域问题

--- src/service/postMarkdownRaw.js
+++ src/service/postMarkdownRaw.js
@@ -4,7 +4,7 @@ let url = status.DOMAIN + '/markdown/raw'
 
 const post = ($http, params) => {
   return new Promise((resolve, reject) => {
-    $http.post(url, params, { headers: { 'Content-Type': 'text/plain' } }).then(
+    $http.post(url, params, { headers: { 'Content-Type': 'text/plain' } }, { emulateJSON: true }).then(
       response => {
         let re = response.body
         if (re) {
